27cda8eac0cca301e646c45b594fabcd2ec43296
Bianca Aguilar <biancamika128@gmail.com>
2025-11-05 01:14:51 +0800

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,15 +3,15 @@ import { getBlogPosts } from 'app/work/utils'
 export const baseUrl = 'https://bianca.digital'
 
 export default async function sitemap() {
-  let blogs = getBlogPosts().map((post) => ({
-    url: `${baseUrl}/blog/${post.slug}`,
+  let posts = getBlogPosts().map((post) => ({
+    url: `${baseUrl}/work/${post.slug}`,
     lastModified: post.metadata.publishedAt,
   }))
 
-  let routes = ['', '/blog'].map((route) => ({
+  let routes = ['', '/work'].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
 
-  return [...routes, ...blogs]
+  return [...routes, ...posts]
 }
