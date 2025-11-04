d1b9eb960cb1ff61996ad82c8ccb7b24bba8f418
Bianca Aguilar <biancamika128@gmail.com>
2025-11-05 00:27:56 +0800

    Updated baseUrl

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import { getBlogPosts } from 'app/work/utils'
 
-export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
+export const baseUrl = 'https://bianca.digital'
 
 export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
